4bd3491d1525ae3d198da03daaed8a99590724cb
Sage Lee <sagemcklee@gmail.com>
2020-03-19 13:26:05 -0600

    Adding more tests to player test file

--- test/player_test.rb
+++ test/player_test.rb
@@ -10,6 +10,7 @@ def test_does_it_exist
 end
 
 def test_it_has_readable_attributes
+  skip
   card1 = Card.new(:diamond, 'Queen', 12)
   card2 = Card.new(:spade, '3', 3)
   card3 = Card.new(:heart, 'Ace', 14)
@@ -20,4 +21,29 @@ def test_it_has_readable_attributes
 
   assert_equal player, Player.deck
   assert_equal player, Player.name
+end
+
+def test_it_can_lose
+  skip
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  card_set = [card1, card2, card3]
+  deck = Deck.new(card_set)
+
+  player = Player.new('Clarisa', deck)
+
+  assert_equal false, player.has_lost?
+end
+
+def test_player_remove_card
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  card_set = [card1, card2, card3]
+  deck = Deck.new(card_set)
+
+  player = Player.new('Clarisa', deck)
+
+assert_equal deck[0], player.deck.remove_card
 end
