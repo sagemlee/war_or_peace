4ec22f122e7ab3019ad3bd460650e218010b6749
Sage Lee <sagemcklee@gmail.com>
2020-03-22 10:20:22 -0600

    Adding turn test does type exitst

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,12 +4,25 @@ require './lib/player'
 
 
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war, :basic, :war, :mutually_assured_destruction
 
   def initialize(player1, player2, spoils_of_war = [])
     @player1 = player1
     @player2 = player2
     @spoils_of_war = spoils_of_war
+    @basic = basic
+    @war = war
+    @mutually_assured_destruction = mutually_assured_destruction
+end
+
+def type
+  if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+    :basic
+  elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    :war
+  elsif (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
+    :mutually_assured_destruction
+  end
 end
 
 end
