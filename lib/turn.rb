11e9e71fa3b143ef21c9d92efa754fafe364c3f2
Sage Lee <sagemcklee@gmail.com>
2020-03-22 11:35:48 -0600

    Adding pile cards test

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,15 +32,31 @@ def winner
     elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
       player2
     end
-  elsif self.type = :war
+  elsif self.type == :war
     if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
       player1
     elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
       player2
     end
-  elsif self.type = mutually_assured_destruction
+  elsif self.type == mutually_assured_destruction
     return "No Winner"
   end
 end
 
+def pile_cards
+  if self.type == :basic
+    @spoils_of_war << player1.deck.cards[0]
+    @spoils_of_war << player2.deck.cards[0]
+    player1.deck.remove_card
+    player2.deck.remove_card
+  elsif self.type == :war
+    @spoils_of_war << player1.deck.cards[0..2]
+    @spoils_of_war << player2.deck.cards[0..2]
+    3.times.player1.deck.remove_card
+    3.times.player2.deck.remove_card
+  elsif self.type == :mutually_assured_destruction
+    3.times.player1.deck.remove_card
+    3.times.player2.deck.remove_card
+  end
+end
 end
