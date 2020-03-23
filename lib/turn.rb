2f4e2430af84b3040fb6fc0d238faf25fa980844
Sage Lee <sagemcklee@gmail.com>
2020-03-22 22:31:40 -0600

    Adding updats to turn test

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,10 +24,10 @@ end
 def type
   if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
     :basic
+  elsif (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(1) == player2.deck.rank_of_card_at(1))
+    :mutually_assured_destruction
   elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
     :war
-  elsif (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
-    :mutually_assured_destruction
   end
 end
 
@@ -51,7 +51,7 @@ def winner
       @loser = player1
     end
     @winner
-  elsif self.type == mutually_assured_destruction
+  elsif self.type == :mutually_assured_destruction
     return "No Winner"
   end
 end
@@ -65,11 +65,19 @@ def pile_cards
   elsif self.type == :war
     @spoils_of_war << player1.deck.cards[0..2]
     @spoils_of_war << player2.deck.cards[0..2]
-    3.times.player1.deck.remove_card
-    3.times.player2.deck.remove_card
+    player1.deck.remove_card
+    player1.deck.remove_card
+    player1.deck.remove_card
+    player2.deck.remove_card
+    player2.deck.remove_card
+    player2.deck.remove_card
   elsif self.type == :mutually_assured_destruction
-    3.times.player1.deck.remove_card
-    3.times.player2.deck.remove_card
+    player1.deck.remove_card
+    player1.deck.remove_card
+    player1.deck.remove_card
+    player2.deck.remove_card
+    player2.deck.remove_card
+    player2.deck.remove_card
   end
 end
 
